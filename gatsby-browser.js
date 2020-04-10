e8a77be620eb794b2298871112484824690584d9
Lauro F. Müller <lauro.muller@lfmuller.com>
2020-04-10 16:02:14 +0200

    chore: install @lauromueller/ui [skip ci]

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import 'swiper/css/swiper.css';
+import '@lauromueller/ui/public/assets/styles/main.scss';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import { Button } from '@lauromueller/ui';
 import { BasicPage } from '../layout';
 
 const Index: FunctionComponent = () => {
-  return <BasicPage>Once again empty</BasicPage>;
+  return (
+    <BasicPage>
+      <Button>Test</Button>
+    </BasicPage>
+  );
 };
 
 export default Index;
